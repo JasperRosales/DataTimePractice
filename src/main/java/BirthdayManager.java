05fba79317aab03db0d3018e990618dbb79b660b
JasperRosales <23-19300@g.batstate-u.edu.ph>
2024-09-19 16:32:42 +0800

    Added more functions to the journey in Date & Time in java

---
+++ src/main/java/BirthdayManager.java
@@ -0,0 +1,23 @@
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class BirthdayManager {
+    private String name;
+    private List<LocalDate> birthdayList = new ArrayList<>();
+
+    public BirthdayManager(){}
+
+    DateFormatter bdayformatter = new DateFormatter();
+
+    public Boolean checkbday() {
+        return null;
+    }
+//
+//    public void setBday(String bday){
+//        bdayformatter.setDate(bday);
+//       birthdayList.add(btime);
+//
+//    }
+}
